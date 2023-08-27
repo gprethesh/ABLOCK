f31571b39471b1f9b2647bcc0a2f5f1386ca339d
Rohit Nishad <r.n.rohitnishad@gmail.com>
2023-08-27 16:24:22 +0530

    Changes

--- receiver.js
+++ receiver.js
@@ -1,111 +1,88 @@
 const Hyperswarm = require("hyperswarm");
-const Block = require("./block.js").Block;
-const BlockHeader = require("./block.js").BlockHeader;
+const { Block, BlockHeader } = require("./block.js");
 const crypto = require("crypto");
 
 const swarm2 = new Hyperswarm();
 
-const difficulty = 25;
-
+const difficulty = 0x1C; // Difficulty represented as hexadecimal
 const maximumTarget =
-  "0x00000FFFFFFF0000000000000000000000000000000000000000000000000000";
-const target = maximumTarget / difficulty;
+  BigInt("0x00000FFFFFFF0000000000000000000000000000000000000000000000000000");
+const target = maximumTarget / BigInt(difficulty);
 
 function calculateHashForBlock(block) {
-  return crypto
-    .createHash("sha256")
-    .update(
-      block.blockHeader.version +
-        block.blockHeader.previousBlockHeader +
-        block.blockHeader.merkleRoot +
-        block.blockHeader.time +
-        block.nonce
-    )
-    .digest("hex");
+  const data = [
+    block.blockHeader.version,
+    block.blockHeader.previousBlockHeader,
+    block.blockHeader.merkleRoot,
+    block.blockHeader.time,
+    block.nonce.toString(),
+  ].join("");
+
+  return crypto.createHash("sha256").update(data).digest("hex");
 }
 
 async function mineBlock(block) {
-  console.log(`target`, target);
+  console.log("target", target.toString());
   let hash = calculateHashForBlock(block);
-  console.log(`Mining for hash`, hash);
-  while (parseInt(hash, 16) > target) {
+  console.log("Mining for hash", hash);
+  while (BigInt("0x" + hash) > target) {
     block.nonce++;
     hash = calculateHashForBlock(block);
   }
 
-  block.blockHeader.difficulty = target;
-
+  block.blockHeader.difficulty = "0x" + difficulty.toString(16); // Difficulty as hexadecimal
   block.blockHeader.hash = hash;
-
   return block;
 }
 
 async function createNewBlock(transactions, minerAddress) {
-  let userTransactions = [...transactions]; // Copy the original transactions first
-
-  let tx = userTransactions.filter(
+  const userTransactions = transactions.filter(
     (transaction) =>
       transaction.sender !== "coinbase" && transaction.sender !== "system"
   );
 
-  let blockHeader = await createNewBlockHeader(transactions);
-  let index = 1;
-  console.log(`INDEX`, index);
+  const blockHeader = await createNewBlockHeader(transactions);
+  const index = 1;
+  console.log("INDEX", index);
 
-  // Use blockTransactions for the transactions of newBlock
-  // And use tx for the tx field of newBlock
-  let newBlock = new Block(blockHeader, index, transactions, tx);
+  const newBlock = new Block(blockHeader, index, transactions, userTransactions);
 
   return newBlock;
 }
 
-// TODO
-
 async function createNewBlockHeader(transactions) {
-  let timestamp = new Date().getTime();
-  let version = "1.0.0";
-  let previousBlockHeader = await "12344567788";
+  const timestamp = new Date().getTime();
+  const version = "1.0.0";
+  const previousBlockHeader = "12344567788"; // Placeholder value
+  const merkleRoot = "67868769868"; // Placeholder value
+  const difficulty = null; // Placeholder value
 
-  let merkleTree = "1578676876678";
-  let merkleRoot = "67868769868";
-  let difficulty = null;
-  return new BlockHeader(
-    version,
-    previousBlockHeader,
-    merkleRoot,
-    timestamp,
-    difficulty
-  );
+  return new BlockHeader(version, previousBlockHeader, merkleRoot, timestamp, difficulty);
 }
 
 const run = async () => {
   const block1 = await createNewBlock([], "hello");
 
-  console.log(`block Created`);
+  console.log("block Created");
 
-  const data = mineBlock(block1);
-  console.log(data);
+  const minedBlock = await mineBlock(block1);
+  console.log(minedBlock);
 
   swarm2.on("connection", (conn, info) => {
-    console.log(`inside`);
-
-    // setInterval(() => {
-    //   const data = mineBlock(block1);
-    //   console.log(data);
-    // }, 10000);
+    console.log("inside");
     conn.on("data", (data) =>
       console.log("client got message:", data.toString())
     );
   });
 
   const callFun = async () => {
-    const topic = Buffer.alloc(32).fill("UPOW"); // A topic must be 32 bytes
+    const topic = Buffer.alloc(32, "UPOW"); // Fixed length buffer
     console.log("Joining topic:", topic.toString());
-    const discovery = swarm2.join(topic, { server: false, client: true }); // Change here
-    await discovery.flushed(); // Waits for the topic to be fully announced on the DHT
+    const discovery = swarm2.join(topic, { server: false, client: true });
+    await discovery.flushed();
     console.log("Topic announced on DHT");
     await swarm2.flush();
-    console.log(`done`);
+    console.log("done");
   };
 
   callFun();
